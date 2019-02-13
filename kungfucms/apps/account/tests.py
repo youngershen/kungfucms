9547936fcaedc9b607fd2b16a3485206847e1496
Younger Shen <younge.x.shen@gmail.com>
2019-02-13 09:56:17 +0800

    feature: add pytest-django to test environment

--- kungfucms/apps/account/tests.py
+++ kungfucms/apps/account/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class RegisterTestCase(TestCase):
+
+    def test_register(self):
+        self.assertTrue(True)
+
--- kungfucms/apps/account/validators.py
+++ kungfucms/apps/account/validators.py
@@ -5,17 +5,31 @@
 # CELL : 13811754531
 # WECHAT : 13811754531
 # WEB : https://youngershen.com
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext as _
 from validator import Validator
 
 
 class UsernameRegister(Validator):
     username = 'required|unique:AUTH_USER_MODEL,username|min_length:5'
     password = 'required|password:middle'
-    password_confirm = 'required'
+    password_confirm = 'required|same:password'
 
     messages = {
-
+        'username': {
+            'required': _('username is required'),
+            'unique': _('username already exists'),
+            'min_length': _('the length of username must greater than 5')
+        },
+        'password': {
+            'required': _('password is required'),
+            'password': _('the password length must longer '
+                          'than 7 and it sould contains lower , '
+                          'upper latin characters and digits')
+        },
+        'password_confirm': {
+            'required': _('password_confirm is required'),
+            'same': _('passwordc_confirm must same as the password field')
+        }
     }
 
 
