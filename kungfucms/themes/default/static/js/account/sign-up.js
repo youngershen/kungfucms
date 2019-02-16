4e46b4d2ab121cfc4af70ab0042002b5a099bd79
Younger Shen <younger.x.shen@gmail.com>
2019-02-16 11:49:42 +0800

    feature: add email check and cellphone check fucntions

--- kungfucms/themes/default/static/js/account/sign-up.js
+++ kungfucms/themes/default/static/js/account/sign-up.js
@@ -2,57 +2,69 @@
 
     let kungfucms = window.kungfucms;
 
-    function check_username(username) {
+    function check_username(username, success_callback, error_callback) {
         kungfucms.ajax({
             'url': '/account/check-username',
             'method': 'post',
             'data': {
                 'username': username
             },
-            'success': function (data) {
-                console.log(data);
-            },
-            'error': function (data) {
-                console.log(data);
-            }
+            'success': success_callback(data),
+            'error': error_callback(data)
         });
     }
 
-    function check_email(email) {
+    function check_email(email, success_callback, error_callback) {
         kungfucms.ajax({
             'url': '/account/check-email',
             'method': 'post',
             'data': {
                 'email': email
             },
-            'success': function (data) {
-                console.log(data);
-            },
-            'error': function (data) {
-                console.log(data);
-            }
+            'success': success_callback(data),
+            'error': error_callback(data)
         });
     }
 
 
-    function check_cellphone(cellphone) {
+    function check_cellphone(cellphone, success_callback, error_callback) {
         kungfucms.ajax({
             'url': '/account/check-cellphone',
             'method': 'post',
             'data': {
                 'cellphone': cellphone
             },
-            'success': function (data) {
-                console.log(data);
-            },
-            'error': function (data) {
-                console.log(data);
-            }
+            'success': success_callback(data),
+            'error': error_callback(data)
+        });
+    }
+
+    function update_submit_button_status()
+    {
+
+    }
+
+    function hook_submit_button_onclick()
+    {
+        let username_field_status = $('#kf-username').attr('status');
+        let username = false;
+
+        if(username_field_status === 'true')
+        {
+            username = true;
+        }
+
+        $('#kf-submit-btn').click(function(){
+
+            console.log(username);
         });
     }
 
     kungfucms.check_username = check_username;
     kungfucms.check_email = check_email;
     kungfucms.check_cellphone = check_cellphone;
+    kungfucms.update_submit_button_status = update_submit_button_status;
+    kungfucms.hook_submit_button_onclick = hook_submit_button_onclick;
+
 
 })(window, $);
