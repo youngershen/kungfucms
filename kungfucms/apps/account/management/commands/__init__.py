046453d77ac6a2ae96b0420b29c8639a5b7159d2
Younger Shen <shenyangang@163.com>
2020-06-08 16:21:58 +0800

    Save: 保存代码

---
+++ kungfucms/apps/account/management/commands/__init__.py
@@ -0,0 +1,7 @@
+# PROJECT : kungfucms
+# TIME : 2018/11/20 10:10
+# AUTHOR : Younger Shen
+# EMAIL : younger.x.shen@gmail.com
+# CELL : 13811754531
+# WECHAT : 13811754531
+# https://github.com/youngershen/
